a3d05c9bfc9b53e945f59339f2b5ce8603735d0c
Sergey Suvorov <suvorov@inr.ru>
2020-11-03 02:39:12 +0100

    update with cats switcher

--- app/static/src/js/settings.js
+++ app/static/src/js/settings.js
@@ -1,21 +1,32 @@
+function addCat(cat) {
+  let parent = document.createElement("div");
+  parent.setAttribute("class", "d-flex");
+  parent.setAttribute("id", "par-cat-"+cat.replaceAll(".", "-"))
+
+  let close = document.createElement("button");
+  close.setAttribute("id", "close_"+cat.replaceAll(".", "-"));
+  close.setAttribute("class", "close close-btn");
+  close.innerHTML = "&times"
+
+  close.onclick = function(event) {
+    $("#par-cat-"+event.target.getAttribute("id").split("_")[1]).removeClass("d-flex");
+    $("#par-cat-"+event.target.getAttribute("id").split("_")[1]).fadeOut();
+    $(".btn").removeClass("disabled");
+    // TODO remove from array
+  }
+
+  let catElement = document.createElement("div");
+  catElement.setAttribute("class", "pl-2");
+  catElement.textContent = allCatsArray[cat];
+
+  document.getElementById("cats-list").appendChild(parent);
+  parent.appendChild(close);
+  parent.appendChild(catElement);
+}
+
 function renderCats() {
   CATS.forEach((cat, num) => {
-    let parent = document.createElement("div");
-    parent.setAttribute("class", "d-flex");
-
-    let close = document.createElement("button");
-    close.setAttribute("id", "close-"+cat);
-    close.setAttribute("class", "close close-btn");
-    close.innerHTML = "&times"
-
-    let catElement = document.createElement("div");
-    catElement.setAttribute("id", "cat-label-"+num);
-    catElement.setAttribute("class", "pl-2");
-    catElement.textContent = allCatsArray[cat];
-
-    document.getElementById("cats-list").appendChild(parent);
-    parent.appendChild(close);
-    parent.appendChild(catElement);
+    addCat(cat);
   });
 }
 
@@ -36,5 +47,17 @@ function validateCat() {
 }
 
 $("#add-cat-btn").click((event) => {
-  alert("Ha")
+  cat = document.forms["add-cat"]["cat_name"].value;
+  // TODO add check if not already
+  if (allCatsArray[cat] !== undefined) {
+    $(".btn").removeClass("disabled");
+    addCat(document.forms["add-cat"]["cat_name"].value);
+    document.forms["add-cat"]["cat_name"].value = "";
+    // TODO add to array
+
+  }
+});
+
+$("#btn-cancel").click((event) => {
+  location.reload();
 });
