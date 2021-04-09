d29d239a8d5b9a4562a07d150ed6aea235673a82
Sergey Suvorov <suvorov@inr.ru>
2021-04-09 20:30:34 +0200

    tests implementation with coverage

--- test/test_auth.py
+++ test/test_auth.py
@@ -1,9 +1,10 @@
 import os
-# import tempfile
 import pytest
 
 from werkzeug.utils import import_string
 from werkzeug.security import generate_password_hash
+
+from flask import session
 
 from app import app_init, db
 from app.model import User
@@ -39,6 +40,28 @@ def test_log(test_client):
                                 )
     assert response.status_code == 200
 
+def test_logout(test_client):
+    response = test_client.post('/login',
+                                data={'i_login': 'tester@gmail.com', 'i_pass':'tester'},
+                                follow_redirects=True
+                                )
+
+    response = test_client.get('/logout',
+                               follow_redirects=True)
+    assert response.status_code == 200
+
+def test_newAcc(test_client):
+    response = test_client.post('/new_user',
+                                data={'email': 'tester2@gmail.com',
+                                      'pasw': 'tester2',
+                                      'pasw2': 'tester2'
+                                      },
+                                      follow_redirects=True
+                                      )
+    assert response.status_code == 200
+    assert 'ERROR' not in response.get_data(as_text=True)
+    assert 'Welcome' in response.get_data(as_text=True)
+
 def test_delAcc(test_client):
     response = test_client.post('/delAcc',
                                 follow_redirects=True
@@ -46,6 +69,11 @@ def test_delAcc(test_client):
     assert response.status_code == 200
 
 def test_change_pass(test_client):
+    response = test_client.post('/login',
+                                data={'i_login': 'tester@gmail.com', 'i_pass':'tester'},
+                                follow_redirects=True
+                                )
+
     response = test_client.post('/change_pasw',
                                 data={'oldPass': 'tester',
                                       'newPass1': 'tester_new',
@@ -54,3 +82,46 @@ def test_change_pass(test_client):
                                 follow_redirects=True
                                 )
     assert response.status_code == 200
+    assert 'ERROR' not in response.get_data(as_text=True)
+    assert 'successfully' in response.get_data(as_text=True)
+
+def test_unauthorised_request(test_client):
+    response = test_client.post('/change_pasw',
+                                data={'oldPass': 'tester',
+                                      'newPass1': 'tester_new',
+                                      'newPass2': 'tester_new'
+                                      },
+                                follow_redirects=True
+                                )
+    assert response.status_code == 200
+    assert 'ERROR' in response.get_data(as_text=True)
+
+def test_change_wrong_pass(test_client):
+    response = test_client.post('/login',
+                                data={'i_login': 'tester@gmail.com', 'i_pass':'tester'},
+                                follow_redirects=True
+                                )
+
+    response = test_client.post('/change_pasw',
+                                data={'oldPass': 'tester_wrong',
+                                      'newPass1': 'tester_new',
+                                      'newPass2': 'tester_new'
+                                      },
+                                follow_redirects=True
+                                )
+    assert response.status_code == 200
+    assert 'ERROR! Wrong' in response.get_data(as_text=True)
+
+    response = test_client.post('/change_pasw',
+                                data={'oldPass': 'tester',
+                                      'newPass1': 'tester_new',
+                                      'newPass2': 'tester_new_other'
+                                      },
+                                follow_redirects=True
+                                )
+    assert response.status_code == 200
+    assert 'don\'t match!' in response.get_data(as_text=True)
+
+def test_load_papers(test_client):
+    response = test_client.get('/load_papers?token=test_token&n_papers=10')
+    assert response.status_code == 201
--- test/test_paper.py
+++ test/test_paper.py
@@ -1,10 +1,70 @@
-from .test_auth import test_client
+import os
+import pytest
+from json import loads
 
-def test_data(test_client):
-    response = test_client.post('/login',
-                                data={'i_login': 'tester@gmail.com', 'i_pass':'tester'},
-                                follow_redirects=True
-                                )
-    response = test_client.get('/load_papers?token=test_token')
-    response = test_client.get('/data?date=today')
+from werkzeug.utils import import_string
+from werkzeug.security import generate_password_hash
+
+from flask import session
+
+from app import app_init, db
+from app.model import User, Paper
+
+
+@pytest.fixture(scope='module', autouse=True)
+def client_with_papers():
+    print('FIXTURE UP')
+    app = app_init()
+    cfg = import_string('configmodule.TestingConfig')
+    app.config.from_object(cfg)
+    with app.app_context():
+        db.create_all()
+        user1 = User(email='tester@gmail.com',
+                 pasw=generate_password_hash('tester'),
+                 arxiv_cat=['hep-ex'],
+                 tags='[{"name": "test", "rule":"ti{test}", "color":"#ff0000"}]',
+                 pref='{"tex": "True"}'
+                 )
+        db.session.add(user1)
+        db.session.commit()
+        client_with_papers = app.test_client()
+        response = client_with_papers.get('/load_papers?token=test_token&n_papers=10&search_query=hep-ex')
+        yield client_with_papers
+        db.session.remove()
+        db.drop_all()
+
+    print('FIXTURE DOWN')
+
+def test_paper_API(client_with_papers):
+    response = client_with_papers.post('/login',
+                                       data={'i_login': 'tester@gmail.com', 'i_pass':'tester'},
+                                       follow_redirects=True
+                                       )
+    response = client_with_papers.get('/data?date=today')
+    data = loads(response.get_data())
     assert response.status_code == 200
+    assert data.get('papers') is not None
+    assert len(data.get('papers')) > 0
+
+
+def test_add_bm(client_with_papers):
+    response = client_with_papers.post('/login',
+                                       data={'i_login': 'tester@gmail.com', 'i_pass':'tester'},
+                                       follow_redirects=True
+                                       )
+    test_paper = Paper.query.order_by(Paper.date_up.desc()).first()
+    response = client_with_papers.post('/add_bm',
+                                       data={'paper_id': test_paper.paper_id}
+                                       )
+    assert response.status_code == 201
+
+def test_del_bm(client_with_papers):
+    response = client_with_papers.post('/login',
+                                       data={'i_login': 'tester@gmail.com', 'i_pass':'tester'},
+                                       follow_redirects=True
+                                       )
+    test_paper = Paper.query.order_by(Paper.date_up.desc()).first()
+    response = client_with_papers.post('/del_bm',
+                                       data={'paper_id': test_paper.paper_id}
+                                       )
+    assert response.status_code == 201
