eaedeb73b386af88427892c992cda4355d6cbba2
Sergey Suvorov <suvorov@inr.ru>
2021-04-11 17:24:15 +0200

    better test coverage

--- test/test_paper.py
+++ test/test_paper.py
@@ -27,23 +27,21 @@ def client_with_papers(init_app):
 
 def test_paper_api(client_with_papers):
     """Test paper download."""
-    response = client_with_papers.post('/login',
-                                       data={'i_login': 'tester@gmail.com', 'i_pass':'tester'},
-                                       follow_redirects=True
-                                       )
     response = client_with_papers.get('/data?date=today')
     data = loads(response.get_data())
     assert response.status_code == 200
     assert data.get('papers') is not None
     assert len(data.get('papers')) > 0
 
+def test_paper_page(client_with_papers):
+    """Test paper page load."""
+    response = client_with_papers.get('/papers',
+                                      follow_redirects=True
+                                      )
+    assert response.status_code == 200
 
 def test_add_bm(client_with_papers):
     """Test bookmark add."""
-    response = client_with_papers.post('/login',
-                                       data={'i_login': 'tester@gmail.com', 'i_pass':'tester'},
-                                       follow_redirects=True
-                                       )
     test_paper = Paper.query.order_by(Paper.date_up.desc()).first()
     response = client_with_papers.post('/add_bm',
                                        data={'paper_id': test_paper.paper_id}
@@ -52,12 +50,13 @@ def test_add_bm(client_with_papers):
 
 def test_del_bm(client_with_papers):
     """Test bookmark delete."""
-    response = client_with_papers.post('/login',
-                                       data={'i_login': 'tester@gmail.com', 'i_pass':'tester'},
-                                       follow_redirects=True
-                                       )
     test_paper = Paper.query.order_by(Paper.date_up.desc()).first()
     response = client_with_papers.post('/del_bm',
                                        data={'paper_id': test_paper.paper_id}
                                        )
     assert response.status_code == 201
+
+def test_bookshelf_page(client_with_papers):
+    """Test bookmark page load."""
+    response = client_with_papers.get('/bookshelf')
+    assert response.status_code == 200
