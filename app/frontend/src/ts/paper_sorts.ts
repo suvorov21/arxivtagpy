4cadc71717fc6e7289a0560ebb3067f66ca881c1
Sergey Suvorov <suvorovsb@gmail.com>
2022-12-16 00:45:20 +0100

    fix sorting issue

--- app/frontend/src/ts/paper_sorts.ts
+++ app/frontend/src/ts/paper_sorts.ts
@@ -15,14 +15,8 @@ const sortFunction = (a:number, b:number,
 
 const sortTags = (papers: Array<Paper>, sortMethod: string): void => {
     papers.sort((a: Paper, b: Paper) => {
-        if (b.tags.length === 0 && a.tags.length !== 0) {
-            return sortMethod === "tag-as" ? -1 : 1;
-        }
-        if (b.tags.length !== 0 && a.tags.length === 0) {
-            return sortMethod === "tag-as" ? 1 : -1;
-        }
-        if (b.tags.length === 0 && a.tags.length === 0) {
-            return -1;
+        if (b.tags.length * a.tags.length === 0 && a.tags.length !== 0) {
+            return a.tags.length === 0 ? -1 : 1;
         }
         return sortFunction(a.tags[0], b.tags[0],
             (new Date(a.date_up).getTime()),
@@ -36,22 +30,23 @@ const sortCats = (papers: Array<Paper>, sortMethod: string): void => {
     papers.sort((a: Paper, b: Paper) => {
         let catA = "";
         let catB = "";
-        for (let id = 0; id < a["cats"].length; id++) {
-            if (__CATS__.includes(a["cats"][`${id}`])) {
-                catA = a["cats"][`${id}`];
+        for (let id = 0; id < a.cats.length; id++) {
+            if (__CATS__.includes(a.cats[`${id}`])) {
+                catA = a.cats[`${id}`];
                 break;
             }
         }
-        for (let id = 0; id < b["cats"].length; id++) {
-            if (__CATS__.includes(b["cats"][`${id}`])) {
-                catB = b["cats"][`${id}`];
+        for (let id = 0; id < b.cats.length; id++) {
+            if (__CATS__.includes(b.cats[`${id}`])) {
+                catB = b.cats[`${id}`];
                 break;
             }
         }
+
         return sortFunction(__CATS__.indexOf(catA), __CATS__.indexOf(catB),
                             new Date(a.date_up).getTime(),
                             new Date(b.date_up).getTime(),
-                            sortMethod === "cat-as"
+                            sortMethod.includes("as")
                             );
     });
 }
